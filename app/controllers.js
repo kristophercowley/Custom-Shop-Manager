4296e94d7c82f4426c74fc9025916167e9a1d82e
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-02-23 15:53:00 -0700

    Dependencies

---
+++ app/controllers.js
@@ -0,0 +1,15 @@
+app.controller('MainController', function($scope){
+    $scope.test = "Hello from main controller!"
+})
+
+app.controller('OrderController', function($scope){
+    $scope.test = "Hello from order controller!"
+})
+
+app.controller('BlankController', function($scope){
+    $scope.test = "Hello from blank controller!"
+})
+
+app.controller('Blank2Controller', function($scope){
+    $scope.test = "Hello from blank2 controller!"
+})
---
+++ app/services.js
@@ -0,0 +1,11 @@
+app.service('OrderService', function(){
+    
+})
+
+app.service('BlankService', function(){
+    
+})
+
+app.service('Blank2Service', function(){
+    
+})
