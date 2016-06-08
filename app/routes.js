0e656f2ea820b6a5f0991fac8ca8cf8eeae5a476
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-06-08 16:20:32 -0600

    Dynamic Order Page

--- app/routes.js
+++ app/routes.js
@@ -1,36 +1,37 @@
 app.config(function ($stateProvider, $urlRouterProvider) {
 
     // Use window.location to find out if im running on localhost and assign paths respectively
+    //       /Custom-Shop-Manager
     $urlRouterProvider.otherwise("/home");
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: '/Custom-Shop-Manager/templates/home.html',
+            templateUrl: ' /Custom-Shop-Manager/templates/home.html',
             controller: 'MainController',
             controllerAs: 'mc'
         })
-        
+
         .state('fullfilled', {
             url: '/fullfilled',
-            templateUrl: '/Custom-Shop-Manager/templates/fullfilled.html',
+            templateUrl: ' /Custom-Shop-Manager/templates/fullfilled.html',
             controller: 'MainController',
             controllerAs: 'mc'
         })
-        
+
         .state('control-panel', {
             url: '/control-panel',
-            templateUrl: '/Custom-Shop-Manager/templates/control-panel.html',
+            templateUrl: ' /Custom-Shop-Manager/templates/control-panel.html',
             controller: 'OrderController',
             controllerAs: 'oc'
-        }) 
-        
+        })
+
         .state('orders', {
             url: '/orders',
             templateUrl: '/templates/orders.html',
             controller: 'OrderController',
             controllerAs: 'oc'
         })
-        
+
         .state('order', {
             url: '/orders:orderId',
             templateUrl: '/templates/order.html',
@@ -44,4 +45,3 @@ app.config(function ($stateProvider, $urlRouterProvider) {
         })
 })
 
-   
